9129fc3c270c68a0b2b1e122c19b4d44837c4b2b
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2025-02-07 17:09:20 +0900

    リファクタリング後

--- main.py
+++ main.py
@@ -1,199 +1,143 @@
-import streamlit as st
-from dotenv import load_dotenv
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain.tools import Tool
-from langchain import SerpAPIWrapper
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
-import tiktoken
-from langchain_openai import ChatOpenAI
-from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
-import constants as ct
-import functions as ft
+"""
+このファイルは、Webアプリのメイン処理が記述されたファイルです。
+"""
 
-# 各種設定
+############################################################
+# ライブラリの読み込み
+############################################################
+from dotenv import load_dotenv
+import logging
+import streamlit as st
+import utils
+from initialize import initialize
+import components as cn
+import constants as ct
+
+
+############################################################
+# 設定関連
+############################################################
 st.set_page_config(
     page_title=ct.APP_NAME
 )
+
 load_dotenv()
 
-# CSSでの細かいスタイリング
+logger = logging.getLogger(ct.LOGGER_NAME)
+
+
+############################################################
+# 初期化処理
+############################################################
+try:
+    initialize()
+except Exception as e:
+    logger.error(f"{ct.INITIALIZE_ERROR_MESSAGE}\n{e}")
+    st.error(utils.build_error_message(ct.INITIALIZE_ERROR_MESSAGE), icon=ct.ERROR_ICON)
+    st.stop()
+
+# アプリ起動時のログ出力
+if not "initialized" in st.session_state:
+    st.session_state.initialized = True
+    logger.info(ct.APP_BOOT_MESSAGE)
+
+
+############################################################
+# 初期表示
+############################################################
+# タイトル表示
+cn.display_app_title()
+
+# サイドバー表示
+cn.display_sidebar()
+
+# 初期表示のAIメッセージ
+cn.display_initial_ai_message()
+
+
+############################################################
+# スタイリング処理
+############################################################
+# 画面装飾を行う「CSS」を記述
 st.markdown(ct.STYLE, unsafe_allow_html=True)
 
-# 初期表示
-st.markdown(f"## {ct.APP_NAME}")
-st.markdown("**※AIエージェントの利用有無**")
 
-col1, col2 = st.columns([1, 3])
-with col1:
-    st.session_state.mode = st.selectbox(label="モード", options=["利用する", "利用しない"], label_visibility="collapsed")
+############################################################
+# チャット入力の受け付け
+############################################################
+chat_message = st.chat_input(ct.CHAT_INPUT_HELPER_TEXT)
 
-with st.chat_message("assistant", avatar="images/ai_icon.jpg"):
-    st.success("こちらは弊社に関する質問にお答えするチャットボットです。上記の回答モードでAIエージェントの利用有無を選択し、画面下部のチャット欄から自由に質問してください。生成AIロボットが自動でお答えいたします。")
-    st.markdown("**【AIエージェントとは】**")
-    st.markdown("質問に対して適切と考えられる回答を生成できるまで、生成AIロボット自身に試行錯誤してもらえる機能です。自身の回答に対して評価・改善を繰り返すことで、より優れた回答を生成できます。")
-    st.warning("AIエージェントを利用する場合、回答生成により多くの時間がかかる可能性があります。まずはAIエージェントなしで回答を生成し、適切な回答が得られなかった場合にAIエージェントありで回答を生成してみてください。")
-    st.caption("※AIエージェントを利用したからといって、必ずしも適切な回答を得られるとは限りません。")
 
-# 初期処理
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+############################################################
+# 会話ログの表示
+############################################################
+try:
+    cn.display_conversation_log()
+except Exception as e:
+    logger.error(f"{ct.CONVERSATION_LOG_ERROR_MESSAGE}\n{e}")
+    st.error(utils.build_error_message(ct.CONVERSATION_LOG_ERROR_MESSAGE), icon=ct.ERROR_ICON)
+    st.stop()
 
-    st.session_state.feedback_yes_flg = False
-    st.session_state.feedback_no_flg = False
-    st.session_state.answer_flg = False
-    st.session_state.dissatisfied_reason = ""
-    st.session_state.feedback_no_reason_send_flg = False
 
-    st.session_state.MAX_ALLOWED_TOKENS = 1000
-    st.session_state.total_tokens = 0
-    st.session_state.chat_history = []
-
-    # 消費トークン数カウント用のオブジェクト準備
-    st.session_state.enc = tiktoken.get_encoding("cl100k_base")
-    # コールバックのハンドラ準備
-    st.session_state.st_callback = StreamlitCallbackHandler(st.container())
-    
-    # テーマごとのChainを作成
-    service_doc_chain = ft.create_rag_chain(".db_service")
-    customer_doc_chain = ft.create_rag_chain(".db_customer")
-    company_doc_chain = ft.create_rag_chain(".db_company")
-    st.session_state.rag_chain = ft.create_rag_chain(".db_all")
-
-    # Toolで実行される関数の定義
-    def run_service_doc_chain(param):
-        ai_msg = service_doc_chain.invoke({"input": param, "chat_history": st.session_state.chat_history})
-        st.session_state.chat_history.extend([HumanMessage(content=param), AIMessage(content=ai_msg["answer"])])
-        return ai_msg["answer"]
-    def run_customer_doc_chain(param):
-        ai_msg = customer_doc_chain.invoke({"input": param, "chat_history": st.session_state.chat_history})
-        st.session_state.chat_history.extend([HumanMessage(content=param), AIMessage(content=ai_msg["answer"])])
-        return ai_msg["answer"]
-    def run_company_doc_chain(param):
-        ai_msg = company_doc_chain.invoke({"input": param, "chat_history": st.session_state.chat_history})
-        st.session_state.chat_history.extend([HumanMessage(content=param), AIMessage(content=ai_msg["answer"])])
-        return ai_msg["answer"]
-
-    # AgentExecutorの作成
-    search = SerpAPIWrapper()
-    tools = [
-        Tool(
-            name = "Web検索Tool",
-            func=search.run,
-            description="自社サービス「HealthX」に関する質問で、Web検索が必要と判断した場合に使う"
-        ),
-        Tool(
-            func=run_service_doc_chain,
-            name="自社サービス「EcoTee」について",
-            description="自社サービス「EcoTee」に関する情報を参照したい時に使う"
-        ),
-        Tool(
-            func=run_customer_doc_chain,
-            name="顧客とのやり取りについて",
-            description="顧客とのやりとりに関する情報を参照したい時に使う"
-        ),
-        Tool(
-            func=run_company_doc_chain,
-            name="自社「株式会社EcoTee」について",
-            description="自社「株式会社EcoTee」に関する情報を参照したい時に使う"
-        )
-    ]
-    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.5, streaming=True)
-    st.session_state.agent_executor = initialize_agent(
-        llm=llm,
-        tools=tools,
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        max_iterations=5,
-        early_stopping_method="generate",
-        handle_parsing_errors=True,
-    )
-
-input_message = st.chat_input("質問を入力してください。")
-
-if st.session_state.feedback_no_flg and input_message:
-    st.session_state.feedback_no_flg = False
-
-for index, message in enumerate(st.session_state.messages):
-    if message["role"] == "assistant":
-        with st.chat_message(message["role"], avatar="images/ai_icon.jpg"):
-            st.markdown(message["content"])
-            # ユーザーフィードバック後のAI側の表示
-            if index == len(st.session_state.messages) - 1:
-                if st.session_state.feedback_yes_flg:
-                    st.caption("ご満足いただけて良かったです！他にもご質問があれば、お気軽にお尋ねください！")
-                    st.session_state.feedback_yes_flg = False
-                if st.session_state.feedback_no_flg:
-                    st.caption("ご期待に添えず申し訳ございません。今後の改善のために、差し支えない範囲でご満足いただけなかった理由を教えていただけますと幸いです。")
-                    st.session_state.dissatisfied_reason = st.text_area("", label_visibility="collapsed")
-                    if st.button("送信"):
-                        st.session_state.feedback_no_flg = False
-                        st.session_state.feedback_no_reason_send_flg = True
-                        st.rerun()
-                if st.session_state.feedback_no_reason_send_flg:
-                    st.session_state.feedback_no_reason_send_flg = False
-                    st.caption("ご回答いただき誠にありがとうございます。")
-    else:
-        with st.chat_message(message["role"], avatar="images/user_icon.jpeg"):
-            st.markdown(message["content"])
-            # ユーザーフィードバック後のユーザー側の表示
-            if index == len(st.session_state.messages) - 1:
-                if st.session_state.feedback_yes_flg:
-                    st.caption("ご満足いただけて良かったです！他にもご質問があれば、お気軽にお尋ねください！")
-                    st.session_state.feedback_yes_flg = False
-                if st.session_state.feedback_no_flg:
-                    st.caption("ご期待に添えず申し訳ございません。今後の改善のために、差し支えない範囲でご満足いただけなかった理由を教えていただけますと幸いです。")
-                    st.session_state.dissatisfied_reason = st.text_area("", label_visibility="collapsed")
-                    if st.button("送信"):
-                        st.session_state.feedback_no_flg = False
-                        st.session_state.feedback_no_reason_send_flg = True
-                        st.rerun()
-                if st.session_state.feedback_no_reason_send_flg:
-                    st.session_state.feedback_no_reason_send_flg = False
-                    st.caption("ご回答いただき誠にありがとうございます。")
-
-if input_message:
+############################################################
+# チャット送信時の処理
+############################################################
+if chat_message:
+    # ==========================================
     # 会話履歴の上限を超えた場合、受け付けない
-    input_tokens = len(st.session_state.enc.encode(input_message))
-    if input_tokens > st.session_state.MAX_ALLOWED_TOKENS:
-        with st.chat_message("assistant", avatar="images/ai_icon.jpg"):
-            st.error(f"入力されたテキストの文字数が受付上限値（{st.session_state.MAX_ALLOWED_TOKENS}）を超えています。受付上限値を超えないよう、再度入力してください。")
-    else:
-        st.session_state.total_tokens += input_tokens
+    # ==========================================
+    # ユーザーメッセージのトークン数を取得
+    input_tokens = len(st.session_state.enc.encode(chat_message))
+    # トークン数が、受付上限を超えている場合にエラーメッセージを表示
+    if input_tokens > ct.MAX_ALLOWED_TOKENS:
+        with st.chat_message("assistant", avatar=ct.AI_ICON_FILE_PATH):
+            st.error(ct.INPUT_TEXT_LIMIT_ERROR_MESSAGE)
+            st.stop()
+    # トークン数が受付上限を超えていない場合、会話ログ全体のトークン数に加算
+    st.session_state.total_tokens += input_tokens
 
-        # ユーザーメッセージの追加と表示
-        st.session_state.messages.append({"role": "user", "content": input_message})
-        with st.chat_message("user", avatar="images/user_icon.jpeg"):
-            st.markdown(input_message)
-        
-        res_box = st.empty()
-        with st.spinner("回答生成中..."):
-            # AIエージェントの実行
-            result = ft.execute_agent_or_chain(input_message, st.session_state.mode, st.session_state.chat_history)
-            st.session_state.messages.append({"role": "assistant", "content": result})
-        
-        # 古い会話履歴を削除
-        response_tokens = len(st.session_state.enc.encode(result))
-        st.session_state.total_tokens += response_tokens
-        while st.session_state.total_tokens > st.session_state.MAX_ALLOWED_TOKENS:
-            removed_message = st.session_state.chat_history.pop(1)
-            removed_tokens = len(st.session_state.enc.encode(removed_message.content))
-            st.session_state.total_tokens -= removed_tokens
+    # ==========================================
+    # 1. ユーザーメッセージの処理
+    # ==========================================
+    logger.info({"message": chat_message})
 
-        # AIメッセージの表示
-        with st.chat_message("assistant", avatar="images/ai_icon.jpg"):
-            st.markdown(result)
-            st.session_state.answer_flg = True
-            st.caption("この回答はお役に立ちましたか？フィードバックをいただくことで、生成AIの回答の質が向上します。")
+    res_box = st.empty()
+    with st.chat_message("user", avatar=ct.USER_ICON_FILE_PATH):
+        st.markdown(chat_message)
+    
+    # ==========================================
+    # 2. LLMからの回答取得
+    # ==========================================
+    res_box = st.empty()
+    with st.spinner(ct.SPINNER_TEXT):
+        result = utils.execute_agent_or_chain(chat_message)
+    
+    # ==========================================
+    # 3. 古い会話履歴を削除
+    # ==========================================
+    utils.delete_old_conversation_log(result)
 
-# ユーザーフィードバックのボタン表示
-if st.session_state.answer_flg:
-    col1, col2, col3 = st.columns([1, 1, 5])
-    with col1:
-        if st.button("はい"):
-            st.session_state.answer_flg = False
-            st.session_state.feedback_yes_flg = True
-            st.rerun()
-    with col2:
-        if st.button("いいえ"):
-            st.session_state.answer_flg = False
-            st.session_state.feedback_no_flg = True
-            st.rerun()
+    # ==========================================
+    # 4. LLMからの回答表示
+    # ==========================================
+    with st.chat_message("assistant", avatar=ct.AI_ICON_FILE_PATH):
+        try:
+            cn.display_llm_response(result)
+
+            logger.info({"message": result})
+        except Exception as e:
+            logger.error(f"{ct.DISP_ANSWER_ERROR_MESSAGE}\n{e}")
+            st.error(utils.build_error_message(ct.DISP_ANSWER_ERROR_MESSAGE), icon=ct.ERROR_ICON)
+            st.stop()
+    
+    # ==========================================
+    # 5. 会話ログへの追加
+    # ==========================================
+    st.session_state.messages.append({"role": "user", "content": chat_message})
+    st.session_state.messages.append({"role": "assistant", "content": result})
+
+
+############################################################
+# 6. ユーザーフィードバックのボタン表示
+############################################################
+cn.display_feedback_button()
---
+++ utils.py
@@ -0,0 +1,262 @@
+"""
+このファイルは、画面表示以外の様々な関数定義のファイルです。
+"""
+
+############################################################
+# ライブラリの読み込み
+############################################################
+import os
+from dotenv import load_dotenv
+import streamlit as st
+import logging
+from langchain_community.document_loaders import PyMuPDFLoader, Docx2txtLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema import HumanMessage, AIMessage
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import chromadb
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
+import constants as ct
+
+
+############################################################
+# 設定関連
+############################################################
+load_dotenv()
+
+
+############################################################
+# 関数定義
+############################################################
+
+def build_error_message(message):
+    """
+    エラーメッセージと管理者問い合わせテンプレートの連結
+
+    Args:
+        message: 画面上に表示するエラーメッセージ
+
+    Returns:
+        エラーメッセージと管理者問い合わせテンプレートの連結テキスト
+    """
+    return "\n".join([message, ct.COMMON_ERROR_MESSAGE])
+
+
+def create_rag_chain(db_name):
+    """
+    引数として渡されたDB内を参照するRAGのChainを作成
+
+    Args:
+        db_name: RAG化対象のデータを格納するデータベース名
+    """
+    logger = logging.getLogger(ct.LOGGER_NAME)
+
+    docs_all = []
+    # AIエージェント機能を使わない場合の処理
+    if db_name == ct.DB_ALL_PATH:
+        folders = os.listdir(ct.RAG_TOP_FOLDER_PATH)
+        # 「data」フォルダ直下の各フォルダ名に対して処理
+        for folder_path in folders:
+            if folder_path.startswith("."):
+                continue
+            # フォルダ内の各ファイルのデータをリストに追加
+            add_docs(f"{ct.RAG_TOP_FOLDER_PATH}/{folder_path}", docs_all)
+    # AIエージェント機能を使う場合の処理
+    else:
+        # データベース名に対応した、RAG化対象のデータ群が格納されているフォルダパスを取得
+        folder_path = ct.DB_NAMES[db_name]
+        # フォルダ内の各ファイルのデータをリストに追加
+        add_docs(folder_path, docs_all)
+    
+    text_splitter = CharacterTextSplitter(
+        chunk_size=ct.CHUNK_SIZE,
+        chunk_overlap=ct.CHUNK_OVERLAP,
+        separator="\n",
+    )
+    splitted_docs = text_splitter.split_documents(docs_all)
+
+    embeddings = OpenAIEmbeddings()
+
+    # すでに対象のデータベースが作成済みの場合は読み込み、未作成の場合は新規作成する
+    if os.path.isdir(db_name):
+        client_settings = chromadb.config.Settings(
+            chroma_db_impl="duckdb+parquet",
+            persist_directory=db_name,
+            anonymized_telemetry=False
+        )
+        db = Chroma(
+            collection_name="langchain_store",
+            embedding_function=embeddings,
+            client_settings=client_settings,
+            persist_directory=db_name,
+        )
+    else:
+        client_settings = chromadb.config.Settings(
+            chroma_db_impl="duckdb+parquet",
+            persist_directory=db_name,
+            anonymized_telemetry=False
+        )
+        db = Chroma(
+            collection_name="langchain_store",
+            embedding_function=embeddings,
+            client_settings=client_settings,
+            persist_directory=db_name,
+        )
+        db.add_documents(documents=splitted_docs, embedding=embeddings)
+        db.persist()
+    retriever = db.as_retriever(search_kwargs={"k": ct.TOP_K})
+
+    question_generator_template = ct.SYSTEM_PROMPT_CREATE_INDEPENDENT_TEXT
+    question_generator_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", question_generator_template),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_template = ct.SYSTEM_PROMPT_INQUIRY
+    question_answer_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", question_answer_template),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    history_aware_retriever = create_history_aware_retriever(
+        st.session_state.llm, retriever, question_generator_prompt
+    )
+
+    question_answer_chain = create_stuff_documents_chain(st.session_state.llm, question_answer_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    return rag_chain
+
+
+def add_docs(folder_path, docs_all):
+    """
+    フォルダ内のファイル一覧を取得
+
+    Args:
+        folder_path: フォルダのパス
+        docs_all: 各ファイルデータを格納するリスト
+    """
+    files = os.listdir(folder_path)
+    for file in files:
+        # ファイルの拡張子を取得
+        file_extension = os.path.splitext(file)[1]
+        # 想定していたファイル形式の場合のみ読み込む
+        if file_extension in ct.SUPPORTED_EXTENSIONS:
+            # ファイルの拡張子に合ったdata loaderを使ってデータ読み込み
+            loader = ct.SUPPORTED_EXTENSIONS[file_extension](f"{folder_path}/{file}")
+        else:
+            continue
+        docs = loader.load()
+        docs_all.extend(docs)
+
+
+def run_company_doc_chain(param):
+    """
+    会社に関するデータ参照に特化したTool設定用の関数
+
+    Args:
+        param: ユーザー入力値
+
+    Returns:
+        LLMからの回答
+    """
+    # 会社に関するデータ参照に特化したChainを実行してLLMからの回答取得
+    ai_msg = st.session_state.company_doc_chain.invoke({"input": param, "chat_history": st.session_state.chat_history})
+    # 会話履歴への追加
+    st.session_state.chat_history.extend([HumanMessage(content=param), AIMessage(content=ai_msg["answer"])])
+
+    return ai_msg["answer"]
+
+def run_service_doc_chain(param):
+    """
+    サービスに関するデータ参照に特化したTool設定用の関数
+
+    Args:
+        param: ユーザー入力値
+
+    Returns:
+        LLMからの回答
+    """
+    # サービスに関するデータ参照に特化したChainを実行してLLMからの回答取得
+    ai_msg = st.session_state.service_doc_chain.invoke({"input": param, "chat_history": st.session_state.chat_history})
+
+    # 会話履歴への追加
+    st.session_state.chat_history.extend([HumanMessage(content=param), AIMessage(content=ai_msg["answer"])])
+
+    return ai_msg["answer"]
+
+def run_customer_doc_chain(param):
+    """
+    顧客とのやり取りに関するデータ参照に特化したTool設定用の関数
+
+    Args:
+        param: ユーザー入力値
+    
+    Returns:
+        LLMからの回答
+    """
+    # 顧客とのやり取りに関するデータ参照に特化したChainを実行してLLMからの回答取得
+    ai_msg = st.session_state.customer_doc_chain.invoke({"input": param, "chat_history": st.session_state.chat_history})
+
+    # 会話履歴への追加
+    st.session_state.chat_history.extend([HumanMessage(content=param), AIMessage(content=ai_msg["answer"])])
+
+    return ai_msg["answer"]
+
+
+def delete_old_conversation_log(result):
+    """
+    古い会話履歴の削除
+
+    Args:
+        result: LLMからの回答
+    """
+    # LLMからの回答テキストのトークン数を取得
+    response_tokens = len(st.session_state.enc.encode(result))
+    # 過去の会話履歴の合計トークン数に加算
+    st.session_state.total_tokens += response_tokens
+
+    # トークン数が上限値を下回るまで、順に古い会話履歴を削除
+    while st.session_state.total_tokens > ct.MAX_ALLOWED_TOKENS:
+        # 最も古い会話履歴を削除
+        removed_message = st.session_state.chat_history.pop(1)
+        # 最も古い会話履歴のトークン数を取得
+        removed_tokens = len(st.session_state.enc.encode(removed_message.content))
+        # 過去の会話履歴の合計トークン数から、最も古い会話履歴のトークン数を引く
+        st.session_state.total_tokens -= removed_tokens
+
+
+def execute_agent_or_chain(chat_message):
+    """
+    AIエージェントもしくはAIエージェントなしのRAGのChainを実行
+
+    Args:
+        chat_message: ユーザーメッセージ
+    
+    Returns:
+        LLMからの回答
+    """
+    logger = logging.getLogger(ct.LOGGER_NAME)
+
+    # AIエージェント機能を利用する場合
+    if st.session_state.mode == ct.AI_AGENT_MODE_ON:
+        # Agent Executorの実行（AIエージェント機能を使う場合は、Toolとして設定した関数内で会話履歴への追加処理を実施）
+        result = st.session_state.agent_executor.invoke({"input": chat_message}, {"callbacks": [st.session_state.st_callback]})
+        response = result["output"]
+    # AIエージェントを利用しない場合
+    else:
+        # RAGのChainを実行
+        result = st.session_state.rag_chain.invoke({"input": chat_message, "chat_history": st.session_state.chat_history})
+        # 会話履歴への追加
+        st.session_state.chat_history.extend([HumanMessage(content=chat_message), AIMessage(content=result["answer"])])
+        response = result["answer"]
+    
+    return response
